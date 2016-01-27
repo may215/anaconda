562265500fa54c6fd45ab1304d4f99b7c8b8b0c6
Meir Shamay <meir.s@mediapplications.com>
2016-01-27 15:12:32 +0200

    forked commit

---
+++ utils.go
@@ -0,0 +1,15 @@
+package anaconda
+
+// Bool returns a new pointer to the given bool value.
+func Bool(v bool) *bool {
+	ptr := new(bool)
+	*ptr = v
+	return ptr
+}
+
+// Float returns a new pointer to the given float64 value.
+func Float(v float64) *float64 {
+	ptr := new(float64)
+	*ptr = v
+	return ptr
+}
